a3ec6e1e41be5ec32e735bf05059da7a47c9cdc5
purwar2804 <utkarshpurwar2804@gmail.com>
2020-04-27 18:01:43 +0530

    node callback and error handling

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-var rect={
-  perimeter: (x,y)=>(2*(x+y)),
-  area: (x,y)=>(x*y)
-}
-
-
-let solverect=(l,b)=>{
-  if(l<=0 && b<=0)
-  {
-    console.log('l and b should be greater then 0');
-  }
-  else{
-    console.log('area of rectengle are:'+rect.area(l,b));
-    console.log('perimeter of rectangle are:'+rect.perimeter(l,b));
-  }
-
+var rect=require('./rectangle.js')
+var solverect=function(l,b){
+	rect(l,b,(err,rectangle)=>{
+		if(err){
+			console.log(err.message);
+		}
+		else{
+			console.log(rectangle.area())
+			console.log(rectangle.perimeter())
+		}
+	})
 }
 solverect(2,3);
